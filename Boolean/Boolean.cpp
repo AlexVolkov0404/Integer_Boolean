3c0aa1b49d68d58abf014666c99b433dd6a4f3cc
Sasha <Sasha@ACER>
2021-09-13 16:28:15 +0300

    Добавьте файлы проекта.

---
+++ Boolean/Boolean.cpp
@@ -0,0 +1,127 @@
+﻿// Boolean.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+//task8
+void checking_number_not_div_2()
+{
+	int numberA;
+	int numberB;
+	cout << "task8"<<endl;
+	cout << "enter your numberA: ";
+	cin >> numberA;
+	cout << "enter your numberB: ";
+	cin >> numberB;
+	if ((numberA % 2!=0) && (numberB % 2!=0)) {
+		cout << "true";
+	}
+	else {
+		cout << "false";
+	}
+	cin.get();
+	cin.get();
+	
+}
+//task15
+void checking_3_nubers_for_2_pozitive()
+{
+	int A;
+	int B;
+	int C;
+	cout << "task15" << endl;
+	cout << "enter your number A: ";
+	cin >> A;
+	cout << "enter your number B: ";
+	cin >> B;
+	cout << "enter your number C: ";
+	cin >> C;
+	if ((A > 0 && B > 0 && C < 0) || (A < 0 && B > 0 && C < 0) || (A < 0 && B > 0 && C > 0)) {
+		cout << "true";
+	}
+	else {
+		cout << "false";
+	}
+	cin.get();
+	cin.get();
+	
+}
+//task22
+void checking_progression_3n()
+{
+	int num;
+	cout << "task15" << endl;
+	cout << "enter your number(3n): ";
+	cin >> num;
+	int a=	num / 100;
+	int c = num % 10;
+	int b = ( num % 100)/10;
+	cout << a << " " << b << " " << c << endl;
+	if (((b == a - 1) && (c == b - 1)) || ((a == b - 1)&& (b == c - 1))) {
+		cout << "true";
+	}
+	else {
+		cout << "false";
+	}
+	cin.get();
+	cin.get();
+
+}
+//task30
+void chech_right_triangle()
+{
+
+	int A;
+	int B;
+	int C;
+	cout << "task30" << endl;
+	cout << "enter your side1: ";
+	cin >> A;
+	cout << "enter your side2: ";
+	cin >> B;
+	cout << "enter your side3: ";
+	cin >> C;
+	if ((A ==B)&&(B==C)&&(C==A)) {
+		cout << "true";
+	}
+	else {
+		cout << "false";
+	}
+	cin.get();
+	cin.get();
+}
+//task33
+void check_triangle()
+{
+	int A;
+	int B;
+	int C;
+	cout << "task30" << endl;
+	cout << "enter your side1: ";
+	cin >> A;
+	cout << "enter your side2: ";
+	cin >> B;
+	cout << "enter your side3: ";
+	cin >> C;
+	if ((A <(C+ B)) && (B <(A+ C)) && (C< (B+A))) {
+		cout << "true";
+	}
+	else {
+		cout << "false";
+	}
+	cin.get();
+	cin.get();
+}
+
+int main()
+{
+	checking_number_not_div_2();//task8
+	checking_3_nubers_for_2_pozitive();//task15
+	checking_progression_3n();//task22
+	chech_right_triangle();//task30
+	check_triangle();//task33
+
+	return 0;
+}
+
+
---
+++ Integer_Boolean/Integer.cpp
@@ -0,0 +1,87 @@
+﻿// Integer_Boolean.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+//task3
+void amount_of_kilobaits()
+{
+    int amount;
+    cout << "task3"<<endl;
+    cout << "enter amount of Baits: ";
+    cin >> amount;
+    amount = amount / 1024;
+    cout << "amount of KBaits: " << amount << endl;
+    cin.get();
+    cin.get();
+  
+
+
+}
+//task8
+void reverse_your_number()
+{
+    int number;
+    cout << "task8" << endl;
+    cout << "enter your number(2n): ";
+    cin >> number;
+    int part1 = (number / 10)-((number % 10)/10);
+    int part2 = number % 10;
+    part2 = part2 * 10;
+    number = part2 + part1;
+    cout << "your reversed number: " << number<<endl;
+    cin.get();
+    cin.get();
+}
+//task 14
+void change_position_of_last_and_first_part_of_the_number()
+{
+    int number;
+    cout << "task14" << endl;
+    cout << "enter your number(3n): ";
+    cin >> number;
+    int part1 = number % 10;
+    part1 = 100 * part1;
+    int part2 = (number / 10) - ((number % 10) / 10);
+    number = part1 + part2;
+    cout << "your changed number: " << number << endl;
+    cin.get();
+    cin.get();
+}
+//task 17
+void find_hundreads()
+{
+    int number;
+    cout << "task17" << endl;
+    cout << "enter your number(4n): ";
+    cin >> number;
+    number = (number % 1000) / 100;
+    cout << "your hundreads: " << number << endl;
+    cin.get();
+    cin.get();
+
+}
+//task30
+void find_century()
+{
+    int year;
+    cout << "task30" << endl;
+    cout << "enter your year: ";
+    cin >> year;
+    int century = (year / 100) + 1;
+    cout << "your century: " << century << endl;
+    cin.get();
+    cin.get();
+
+}
+int main()
+{
+    amount_of_kilobaits();//task3
+    reverse_your_number();//task8
+    change_position_of_last_and_first_part_of_the_number();//task 14
+    find_hundreads();//task 17
+    find_century();//task30
+    return 0;
+}
+
